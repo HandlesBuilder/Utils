d989db1498c7f164e5b35d95184a4456bacfa1e9
TserHub <jserhub@icloud.com>
2020-06-22 22:53:30 +0800

    --remove: comments

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-/*
- * @Author: Tser
- * @Date: 2020-03-28 22:01:28
- * @GitHub: https://github.com/TserHub
- * @LastEditors: Tser
- * @LastEditTime: 2020-05-14 14:43:39
- */
 import moment from "moment";
 
 export const transformDateTime = (t, hasTime = true) => {
-  // 字符串类型日期转换为moment类型，moment类型的日期，无需转换
   const time = typeof t === "string" ? moment(new Date(t)) : t;
   return time.format(hasTime ? "YYYY-MM-DD HH:mm:ss" : "YYYY-MM-DD");
 };
@@ -69,17 +61,11 @@ export const downloadLink = (url, fileName = undefined) => {
 };
 
 export const jobStatus = {
-  // 未处理
   unhandle: "1",
-  // 导入数据库
   importDb: "10",
-  // 导入完成，可以开始处理账单
   importDone: "13",
-  // 开始处理账单
   handleBill: "15",
-  // 处理完毕，可以开始下载明细
   handleDone: "30",
-  // 处理完毕
   handleOk: "100",
 };
 
