906bac9149143a27a42f17557137aea9bc4e7ac4
docker-soga <30787150+docker-soga@users.noreply.github.com>
2021-08-30 15:56:19 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,83 +1,28 @@
-import moment from "moment";
-
-export const transformDateTime = (t, hasTime = true) => {
-  const time = typeof t === "string" ? moment(new Date(t)) : t;
-  return time.format(hasTime ? "YYYY-MM-DD HH:mm:ss" : "YYYY-MM-DD");
+// 防抖(debounce): 指触发事件后 n 秒后才执行函数，如果在 n 秒内又触发了事件，则会重新计算函数执行时间。
+export const useDebounce = (callback, delay) => {
+  let timer = null;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback.apply(this, args);
+    }, delay);
+  };
 };
 
-export const transTime = (time, flag) => {
-  switch (flag) {
-    case "start":
-      return `${time} 00:00:00`;
-    case "end":
-      return `${time} 23:59:59`;
-    default:
-      return time;
-  }
-};
-
-export const isAccept = (acceptType, fileName) => {
-  return acceptType.split(",").some((type) => {
-    const regExp = new RegExp(`\\${type}$`);
-    return regExp.test(fileName);
-  });
-};
-
-// 校验 min-max 整数
-export const isIntValue = (val, { min, max }) => {
-  const regex = new RegExp(`^[0-9]{${min},${max}}$`);
-  return regex.test(val);
-};
-
-// 校验小数
-export const isDecimalValue = (val) => {
-  return /^\d+(\.\d+)?$/.test(val);
-};
-
-// 校验数值
-export const isNumericValue = (val) => {
-  return /^[+-]?\d+(\.\d+)?$/.test(val);
-};
-
-// 校验绝对值区间
-export const isAbsRangeValue = (val, { min, max }) => {
-  const value = Math.abs(val);
-  return min <= value && value <= max;
-};
-
-// 校验 0 值
-export const isZeroValue = (val) => {
-  return Number(val) === 0;
-};
-
-export const downloadLink = (url, fileName = undefined) => {
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = fileName ?? "";
-  a.target = "_blank";
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-};
-
-export const jobStatus = {
-  unhandle: "1",
-  importDb: "10",
-  importDone: "13",
-  handleBill: "15",
-  handleDone: "30",
-  handleOk: "100",
+// 节流(throttle): 指连续触发事件，但是在 n 秒内，只执行一次函数。
+export const useThrottle = (callback, delay) => {
+  let canRun = true;
+  return (...args) => {
+    if (!canRun) return;
+    canRun = false;
+    setTimeout(() => {
+      callback.apply(this, args);
+      canRun = true;
+    }, delay);
+  };
 };
 
 export default {
-  transformDateTime,
-  transTime,
-  isAccept,
-  isIntValue,
-  isDecimalValue,
-  isNumericValue,
-  isAbsRangeValue,
-  isZeroValue,
-  downloadLink,
-  jobStatus,
-};
+  useDebounce,
+  useThrottle
+}
